1812bc0208b9850f9005067ae7a63746b5319552
AlenaMalyukova <alena.kinko@gmail.com>
2023-01-23 09:36:16 +0300

    public

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <!-- header -->
+    <div class="wrapper-content">
+      <div class="view">
+        <div class="container">
+          <Form @onSubmit="handleSubmit"/>
+          <TweetsList :items="items"/>
+        </div>
+      </div>
+    </div>
+    <!-- footer -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './assets/scss/main.scss'
+import TweetsList from '@/components/TweetsList.vue';
+import Form from './components/Form.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TweetsList,
+    Form
+  },
+  data: () => ({
+    items: [
+      {
+        id: 1,
+        body: 'hello vue 3',
+        likes: 12,
+        avatar: 'https://avatars.dicebear.com/api/male/1.svg',
+        date: new Date(Date.now()).toLocaleString()
+      },
+      {
+        id: 2,
+        body: 'hello world',
+        likes: 6,
+        avatar: 'https://avatars.dicebear.com/api/male/2.svg',
+        date: new Date(Date.now()).toLocaleString()
+      }
+    ]
+  }),
+  methods: {
+    handleSubmit(item) {
+      this.items.push(item)
+    }
   }
 }
 </script>
---
+++ src/components/TweetsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="tweets-wrapper">
+    <Item v-for="item in items" :key="item.id" :item="item"/>
+  </ul>
+</template>
+
+<script>
+import Item from '@/components/Item.vue'
+
+export default {
+  name: 'TweetsList',
+  components: {
+    Item
+  },
+  props: {
+    items: {
+      type: Array,
+      reqired: true
+    }
+  },
+  
+}
+</script>
